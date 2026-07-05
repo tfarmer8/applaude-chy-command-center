24d66221a8c8050d62c143c9b9d7677ad343525e
Tony Farmer <tony@luxara.global>
2026-07-05 14:26:44 -0700

    Applaude build update: src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { KPIDashboard } from './components/KPIDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <KPIDashboard />
+    </div>
+  );
+}
+
+export default App;
